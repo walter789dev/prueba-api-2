1d75a28bda756e532aa23ec7f3610781541f1b40
Martyn Plaza <shuke656@gmail.com>
2023-05-16 12:25:55 -0300

    Actualizando

--- index.js
+++ index.js
@@ -1,42 +1,6 @@
-const express = require('express')
-const app = express()
-const {notes} = require('./notes.json')
+const app = require('./src/app');
+const app = express();
 
-const generateId = () =>{
-   const maxId = Math.max(...notes.map(n => n.id))
-   return maxId + 1
-}
+const PORT = process.env.PORT || 3001;
 
-app.use(express.json())
-
-app.get('/api/notes', (request, response) => response.send(notes))
-app.get('/api/notes/:id', (request, response) => {
-   const id = request.params.id
-   const note = notes.find(note => note.id === Number(id))
-   note ? response.json(note) : response.status(404).end()
- })
-
-app.post('/api/notes', (request, response) => {
-   const body = request.body
-
-   if (!body.content) return response.status(400).json({error: "content missing"})
-
-   const note = {
-      id: generateId(),
-      content: body.content,
-      important: body.important || false,
-      date: new Date()
-   }
-
-   notes.push(note)
-   response.send(note)
- })
-
-app.delete('/api/notes/:id', (request, response) => {
-   const id = request.params.id
-   notes = notes.filter(note => note.id !== Number(id))
-   response.status(204).end()
- })
-
-const PORT = process.env.PORT || 3001
-app.listen(PORT)
+app.listen(PORT);
